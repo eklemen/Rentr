21bbd25929909500d1679903d238e4d10b1e2b50
Aaron Jones <aaronjoneswho@gmail.com>
2015-03-11 17:58:45 -0500

    Added data to the creation test for rentableObject

--- RentrApp/tests.py
+++ RentrApp/tests.py
@@ -9,7 +9,13 @@ class RentableObjectTestCase(APITestCase):
     # Tests creating a rentableObject with a POST
     def test_create_rentable_oject(self):
         url = reverse('rentableObjectList')
-        data = {'': ''}
+        data = {'store': 'Hooters',
+                'type': 'WaveRunner',
+                'isRented': 'true',
+                'dateRented': '2015/03/09',
+                'dateDue': '2015/03/10',
+                'dateReturned': '2015/03/10',
+                'price': '50.00'}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data, data)
