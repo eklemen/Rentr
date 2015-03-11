86e14c5670c020e13010ebd05da66a8f7b354a40
Aaron Jones <aaronjoneswho@gmail.com>
2015-03-11 17:36:04 -0500

    Added the rest_framework package, RentableObjectSerializer, and started on the views for the basic rentableObject.

--- RentrApp/serializers.py
+++ RentrApp/serializers.py
@@ -3,4 +3,9 @@ from rest_framework import serializers
 class RentableObjectSerializer(serializers.Serializer):
     class Meta:
         model = RentableObjectSerializer
-        fields = ()
+        fields = ('store', 'type', 'isRented', 'dateRented', 'dateDue', 'dateReturned', 'price')
+
+class StoreSerializer(serializers.Serializer):
+    class Meta:
+        model = StoreSerializer
+        fields = ('name', 'address', 'isRented', 'phoneNum')
