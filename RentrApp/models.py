bfe08f1e4d28a2941a22500d91389387905b7293
Bipol Alam <bipolalam@gmail.com>
2015-03-11 17:32:09 -0500

    added models

--- RentrApp/models.py
+++ RentrApp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+MAX_SIZE = 100
+
+class RentableObject(models.Model):
+    type = models.CharField(max_length=MAX_SIZE,default='No Type')
+    isRented = models.BooleanField(default=False)
+    dateRented = models.DateTimeField(null=True)
+    dateDue = models.DateTimeField(null=True)
+    dateReturned = models.DateTimeField(null=True)
+    image = models.ImageField(null=True)
+    price = models.FloatField(default=0.00)
+    store = models.ForeignKey('Store', null=True)
+    
+class Store(models.Model):
+    name = models.CharField(max_length=MAX_SIZE,default='No Type')
+    address = models.CharField(max_length=MAX_SIZE,default='No Type')
+    phoneNum = models.CharField(max_length=MAX_SIZE,default='No Type')
+
