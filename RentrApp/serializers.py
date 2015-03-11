adff169b77f9e2ac08c021458ff750b76f984c7b
Bipol Alam <bipolalam@gmail.com>
2015-03-11 17:44:36 -0500

    fixed aarons fuck up

--- RentrApp/serializers.py
+++ RentrApp/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
+from RentrApp.models import RentableObject, Store
 
 class RentableObjectSerializer(serializers.Serializer):
     class Meta:
-        model = RentableObjectSerializer
+        model = RentableObject
         fields = ('store', 'type', 'isRented', 'dateRented', 'dateDue', 'dateReturned', 'price')
 
 class StoreSerializer(serializers.Serializer):
     class Meta:
-        model = StoreSerializer
+        model = Store
         fields = ('name', 'address', 'isRented', 'phoneNum')
