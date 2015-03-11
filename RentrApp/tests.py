c627c6c8ccb11b044e6b0c406b2a0aaabbd5513d
Bipol Alam <bipolalam@gmail.com>
2015-03-11 18:36:49 -0500

    fixed tests

--- RentrApp/tests.py
+++ RentrApp/tests.py
@@ -1,21 +1,23 @@
 from django.core.urlresolvers import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
+from datetime import datetime
+import json
 
 # Create your tests here.
 
 class RentableObjectTestCase(APITestCase):
 
     # Tests creating a rentableObject with a POST
-    def test_create_rentable_oject(self):
+    def test_create_rentable_object(self):
         url = reverse('rentableObjectList')
-        data = {'store': 'Hooters',
-                'type': 'WaveRunner',
-                'isRented': 'true',
-                'dateRented': '2015/03/09',
-                'dateDue': '2015/03/10',
-                'dateReturned': '2015/03/10',
-                'price': '50.00'}
+        data = {'store': None,
+                'type': unicode('WaveRunner'),
+                'isRented': True,
+                'dateRented': unicode('2015-03-11T23:29:56.947000Z'),  
+                'dateDue': unicode('2015-03-11T23:29:56.947000Z'),     
+                'dateReturned': unicode('2015-03-11T23:29:56.947000Z'), 
+                'price': 50.00}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data, data)
