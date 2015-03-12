376aa465e50cfa3b9c1d2eedac8d131424bb061a
Aaron Jones <aaronjoneswho@gmail.com>
2015-03-11 22:42:10 -0500

    Created three new tests test_create_invalid_rentable, test_rentable_list, test_empty_rentable_list. Added some output to the tests so you can tell what is being tested.

--- Rentr/urls.py
+++ Rentr/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from rest_framework import views
+from RentrApp import views
 
 urlpatterns = patterns('',
     # Examples:
--- RentrApp/tests.py
+++ RentrApp/tests.py
@@ -1,8 +1,7 @@
 from django.core.urlresolvers import reverse
+from setuptools.compat import unicode
 from rest_framework import status
 from rest_framework.test import APITestCase
-from datetime import datetime
-import json
 
 # Create your tests here.
 
@@ -10,6 +9,9 @@ class RentableTestCase(APITestCase):
 
     # Tests creating a rentable with a POST
     def test_create_rentable(self):
+        print("********************************************")
+        print("Test the creation of a rentable using a POST")
+        print("********************************************")
         url = reverse('rentableList')
         data = {'store': None,
                 'type': unicode('WaveRunner'),
@@ -20,10 +22,16 @@ class RentableTestCase(APITestCase):
                 'price': 50.00}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        print("Expected Status Code %s" % (status.HTTP_201_CREATED))
+        print("Returned Status Code %s" % (response.status_code))
         self.assertEqual(response.data, data)
+        print(response.data)
 
     # Negative test for creating a rentable
     def test_create_invalid_rentable(self):
+        print("*****************************************************")
+        print("Test the creation of an invalid rentable using a POST")
+        print("*****************************************************")
         url = reverse('rentableList')
         data = {'store': '',
                 'type': '',
@@ -34,4 +42,36 @@ class RentableTestCase(APITestCase):
                 'price': ''}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        print("Expected Status Code %s" % (status.HTTP_400_BAD_REQUEST))
+        print("Returned Status Code %s" % (response.status_code))
 
+    # Using a GET returns a list of all objects
+    def test_rentable_list(self):
+        url = reverse('rentableList')
+        print("***********************************")
+        print("Test using a GET to get all objects")
+        print("***********************************")
+        for x in range(0, 5):
+            data = {'store': None,
+                'type': unicode('WaveRunner'),
+                'isRented': True,
+                'dateRented': unicode('2015-03-11T23:29:56.947000Z'),
+                'dateDue': unicode('2015-03-11T23:29:56.947000Z'),
+                'dateReturned': unicode('2015-03-11T23:29:56.947000Z'),
+                'price': x}
+            response = self.client.post(url, data, format='json')
+        getResponse = self.client.get(url, format='json')
+        print("Number of returned objects")
+        print("Expected 5")
+        print("Returned %s" % (len(getResponse.data)))
+        self.assertEqual(len(getResponse.data), 5)
+
+    # Using a Get when no rentables are saved returns an empty array
+    def test_empty_rentable_list(self):
+        url = reverse('rentableList')
+        print("**********************************************")
+        print("Test using a GET to get an empty rentable list")
+        print("**********************************************")
+        getResponse = self.client.get(url, format='json')
+        print("Expected []")
+        print("Returned %s" % (getResponse.data))
