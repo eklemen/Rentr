1b848f22cce8af1dc31157ca33be2e85713cb111
Bipol Alam <bipolalam@gmail.com>
2015-03-13 16:50:04 -0500

    added templates

--- Rentr/urls.py
+++ Rentr/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 from django.contrib import admin
 from RentrApp import views
+from Rentr import views as auth_views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'Rentr.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', auth_views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'rentable/$', views.RentableList.as_view(), name='rentableList'),
     url(r'rentable/(?P<pk>[0-9]+)/$', views.RentableDetail.as_view(), name='rentable'),
 )
+
