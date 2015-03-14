1d0e5c26139ddb44bc65c116ec8418e0538b48a6
Bipol Alam <bipolalam@gmail.com>
2015-03-13 20:59:25 -0500

    damn it

---
+++ Rentr/views.py
@@ -0,0 +1,8 @@
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponse
+
+def index(request):
+    return render_to_response('rentr/index.html')
