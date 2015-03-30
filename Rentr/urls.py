2020e1904f2e402e29bdc8626dcf42785e6dde24
Bipol Alam <bipolalam@gmail.com>
2015-03-30 18:12:07 -0500

    Fixed iss #8 - added login'

--- Rentr/urls.py
+++ Rentr/urls.py
@@ -10,7 +10,8 @@ urlpatterns = patterns('',
     url(r'^$', auth_views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'rentable/$', views.RentableList.as_view(), name='rentableList'),
+    url(r'^login/$', auth_views.user_login, name="login"), 
     url(r'rentable/(?P<pk>[0-9]+)/$', views.RentableDetail.as_view(), name='rentable'),
     url(r'store/(?P<pk>[0-9]+)/$', views.StoreDetail.as_view(), name='store'),
-)
+    )
 
--- Rentr/views.py
+++ Rentr/views.py
@@ -4,5 +4,36 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, HttpResponse
 
+@login_required
 def index(request):
     return render_to_response('rentr/index.html')
+
+def user_login(request):
+    context = RequestContext(request)
+    print "FUCK"
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/')
+            else:
+                error = {'error': "Your account is disabled."}
+                print "FUCK"
+                return render_to_response('registration/login.html', error, context)
+        else:
+            error = {'error': "Invalid username and/or password"}
+            return render_to_response('registration/login.html', error, context)
+
+    else:
+        return render_to_response('registration/login.html', {}, context)
+
+
+@login_required
+def user_logout(request):
+        logout(request)
+        return HttpResponseRedirect('/')
