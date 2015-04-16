9ac73fb11f0749b8f8bb654cc67dc1c020fdfc0e
shiguna <talorjoy@gmail.com>
2015-04-16 13:27:19 -0500

    Testing with 'store.pk'

--- RentrApp/serializers.py
+++ RentrApp/serializers.py
@@ -4,12 +4,12 @@ from RentrApp.models import Rentable, Store, Rental
 class RentableSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rentable
-        fields = ('type', 'isRented', 'dateRented', 'dateDue', 'dateReturned', 'store')
+        fields = ('store.pk', 'type', 'isRented', 'dateRented', 'dateDue', 'dateReturned')
 
 class StoreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Store
-        fields = ('name', 'address', 'phoneNum')
+        fields = ('pk', 'name', 'address', 'phoneNum')
 
 class RentalSerializer(serializers.ModelSerializer):
     class Meta:
