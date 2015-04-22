b6cf073a6ee365b44801beb33742118fa2a11291
Evan Klemen <ejklemen@gmail.com>
2015-04-22 11:19:30 -0400

    Finally got the damned object for rentables

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('rentr', ['ngResource']);
+var app = angular.module('rentr', ['ngResource', 'restangular']);
 
 // changes syntax of angular so its not the same as django
 app.config(function($interpolateProvider) {
@@ -9,15 +9,24 @@ app.config(function($interpolateProvider) {
 });
 
 // factory for api calls
-app.factory('rentFactory', function($resource){
-//    return $resource('/rentable.json');
+//app.factory('rentFactory', function($resource){
 
-});
+//});
 
 // main controller
-app.controller('MainController', function($scope) {
+app.controller('MainController', function($scope, Restangular) {
     $scope.world = 'World';
     $scope.names = ['foo', 'bar', 'baz', 'bat'];
     
-//    $scope.foo = rentFactory.query();
+    var total = Restangular.all('rentable', '/rentable').getList();
+
+    console.log(total);
+    
+    var rentable = [];
+    var totes = Restangular.all("rentable/");
+    $scope.rentable = totes.getList().then(function(rentable){
+        console.log(rentable[0]);
+        return rentable;
+    })
+    console.log($scope.rentable);
 });
